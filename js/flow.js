8a615a499de096c71df9d40c7e4a92ec6efecb9f
Saran Chockan <saranjitharunesh@gmail.com>
2018-06-18 02:16:08 -0500

    added mousetrap functionality and script to switch tabs

--- js/flow.js
+++ js/flow.js
@@ -1,7 +1,39 @@
 var Mousetrap = require('mousetrap');
 
+//-- Switching Tabs
+const tabs = document.getElementById('flow-navbar').getElementsByClassName('nav-link');
+var index = 0;
+
 
 //-- Mousetrap Script: Keybindings for Tab customziation
-Mousetrap.bind('command+left', function() { console.log('switch to previous tab'); });
+Mousetrap.bind('command+left', function() { 
 
-Mousetrap.bind('command+right', function() { console.log('switch to next tab'); });
+    if(index>0){
+        tabs[index].className = "nav-link";
+        tabs[index-1].className = "nav-link active";
+        index = index - 1;
+    }
+    else{
+        tabs[index].className = "nav-link";
+        tabs[tabs.length-1].className = "nav-link active";
+        index = tabs.length-1;
+    }
+    
+});
+Mousetrap.bind('command+right', function() { 
+
+    if(index<tabs.length-1){
+        tabs[index].className = "nav-link";
+        tabs[index+1].className = "nav-link active";
+        index = index + 1;
+    }
+    
+    else{
+        tabs[index].className = "nav-link";
+        tabs[0].className = "nav-link active";
+        index = 0;
+    }
+    
+});
+
+
