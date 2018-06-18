a803b3dc665e7cf847ecc5da7c6661cf07350446
Saran Chockan <saranjitharunesh@gmail.com>
2018-06-18 11:52:00 -0500

    added aria-selected attribute to tabs

--- js/flow.js
+++ js/flow.js
@@ -2,16 +2,19 @@ var Mousetrap = require('mousetrap');
 
 //-- Switching Tabs
 const tabs = document.getElementById('flow-navbar').getElementsByClassName('nav-link');
-var index = 0;
 
+
+var index = 0;
 
 //-- Mousetrap Script: Keybindings for Tab customziation
 Mousetrap.bind('command+left', function() { 
 
     if(index>0){
+        
         tabs[index].className = "nav-link";
         tabs[index-1].className = "nav-link active";
         index = index - 1;
+        
     }
     else{
         tabs[index].className = "nav-link";
@@ -35,5 +38,17 @@ Mousetrap.bind('command+right', function() {
     }
     
 });
+
+// Mouse Click Test Case
+
+/*
+document.getElementById('test').addEventListener("click",()=>{
+    console.log('test');
+
+    for(i = 0;i<tabs.length;i++){
+        console.log(i + " " + tabs[i].className);
+    }
+});
+*/
 
 
