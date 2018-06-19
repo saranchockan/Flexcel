add0861083d44fc5272c16f9f769d4f537b54fbb
Saran Chockan <saranjitharunesh@gmail.com>
2018-06-19 12:12:48 -0500

    added handstonable cs and js files

--- js/flow-template/1AC -flow.js
+++ js/flow-template/1AC -flow.js
@@ -1,28 +1,30 @@
 
-
 var test_tab = document.getElementById('1AC-Framing-tab');
-
 var flow = new Handsontable(test_tab, {
-        height: 456,
-        colWidths: 105,
-        minCols: 5,
-        minRows: 500,
-        rowHeaders: true,
-        colHeaders: [
-            '1AC',
-            '1NR',
-            '1AR',
-            '2NR',
-            '2AR',
-        ],
-        columnSorting: true,
-        filters: true,
-        dropdownMenu: true,
-        contextMenu: true,
-        autoRowSize: true,
-        manualColumnMove: true,
-        manualRowMove: true,
-        fillHandle: {
-            autoInsertRow: false,
-        }
-    });
+    height: 456,
+    colWidths: 105,
+    minCols: 5,
+    minRows: 500,
+    rowHeaders: true,
+    colHeaders: [
+        '1AC',
+        '1NR',
+        '1AR',
+        '2NR',
+        '2AR',
+    ],
+    columnSorting: true,
+    filters: true,
+    dropdownMenu: true,
+    contextMenu: true,
+    autoRowSize: true,
+    manualColumnMove: true,
+    manualRowMove: true,
+    fillHandle: {
+        autoInsertRow: false,
+    }
+});
+
+
+
+
