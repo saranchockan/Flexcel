db34c283f67e1f47144dde4eee6b86baa19fb52e
Saran Chockan <saranjitharunesh@gmail.com>
2018-06-19 12:35:26 -0500

    fixed handstonable bug

--- js/flow-template/1AC -flow.js
+++ js/flow-template/1AC -flow.js
@@ -1,30 +1,32 @@
 
-var test_tab = document.getElementById('1AC-Framing-tab');
-var flow = new Handsontable(test_tab, {
-    height: 456,
-    colWidths: 105,
-    minCols: 5,
-    minRows: 500,
-    rowHeaders: true,
-    colHeaders: [
-        '1AC',
-        '1NR',
-        '1AR',
-        '2NR',
-        '2AR',
-    ],
-    columnSorting: true,
-    filters: true,
-    dropdownMenu: true,
-    contextMenu: true,
-    autoRowSize: true,
-    manualColumnMove: true,
-    manualRowMove: true,
-    fillHandle: {
-        autoInsertRow: false,
-    }
-});
+var flow_tabs = document.getElementsByClassName('1AC');
+var flow;
 
-
+for(i = 0;i<tabs.length;i++){
+    flow = new Handsontable(flow_tabs[i], {
+        height: 456,
+        colWidths: 105,
+        minCols: 5,
+        minRows: 500,
+        rowHeaders: true,
+        colHeaders: [
+            '1AC',
+            '1NR',
+            '1AR',
+            '2NR',
+            '2AR',
+        ],
+        columnSorting: true,
+        filters: true,
+        dropdownMenu: true,
+        contextMenu: true,
+        autoRowSize: true,
+        manualColumnMove: true,
+        manualRowMove: true,
+        fillHandle: {
+            autoInsertRow: false,
+        }
+    });
+}
 
 
