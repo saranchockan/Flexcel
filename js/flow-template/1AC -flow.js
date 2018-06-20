be033e3c17d4cc174e471d13c9795e8098a42331
Saran Chockan <saranjitharunesh@gmail.com>
2018-06-20 18:10:49 -0500

    changed tab cell width

--- js/flow-template/1AC -flow.js
+++ js/flow-template/1AC -flow.js
@@ -2,52 +2,52 @@
 
 $('#1AC-Framing-tab').jexcel({
     colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 100, 100, 100,100, 100, 100 ]
+    colWidths: [ 165, 165, 165,165, 165, 165 ]
     
 });
 
 $('#Inherency-tab').jexcel({
     colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 100, 100, 100,100, 100, 100 ]
+    colWidths: [ 165, 165, 165,165, 165, 165 ]
     
 });
 
 
 $('#Adv-1-tab').jexcel({
     colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 100, 100, 100,100, 100, 100 ]
+    colWidths: [ 165, 165, 165,165, 165, 165 ]
     
 });
 
 
 $('#Adv-2-tab').jexcel({
     colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 100, 100, 100,100, 100, 100 ]
+    colWidths: [ 165, 165, 165,165, 165, 165 ]
     
 });
 
 
 $('#Adv-3-tab').jexcel({
     colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 100, 100, 100,100, 100, 100 ]
+    colWidths: [ 165, 165, 165,165, 165, 165 ]
     
 });
 
 $('#Adv-4-tab').jexcel({
     colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 100, 100, 100,100, 100, 100 ]
+    colWidths: [ 165, 165, 165,165, 165, 165 ]
     
 });
 
 $('#Adv-5-tab').jexcel({
     colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 100, 100, 100,100, 100, 100 ]
+    colWidths: [ 165, 165, 165,165, 165, 165 ]
     
 });
 
 $('#Solvency-tab').jexcel({
     colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 100, 100, 100,100, 100, 100 ]
+    colWidths: [ 165, 165, 165,165, 165, 165 ]
     
 });
 
