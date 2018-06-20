4241426eb5835da54f414f5b8655a27b3cbf0e6e
Saran Chockan <saranjitharunesh@gmail.com>
2018-06-19 22:55:53 -0500

    fixed tab ux bug

--- js/ld/plan/plan-flow.js
+++ js/ld/plan/plan-flow.js
@@ -9,14 +9,13 @@ var index = 0;
 
 Mousetrap.bind('command+o', function () {
 
-    console.log(getClassNames());
     if (index > 0) {
         
         var id = tabs[index-1].id;
         var reference = '#' + id;
         $(document).ready(function () {
             $(reference).click();
-        },'keyup');
+        });
 
         index = index - 1;
 
@@ -27,19 +26,16 @@ Mousetrap.bind('command+o', function () {
         var reference = '#' + id;
         $(document).ready(function () {
             $(reference).click();
-        },'keyup');
+        });
         index = tabs.length - 1;
 
     }
 
-    switchFlow();
 
 
 });
 
 Mousetrap.bind('command+p', function () {
-
-    console.log(getClassNames());
 
     if (index < tabs.length - 1) {
 
@@ -47,7 +43,7 @@ Mousetrap.bind('command+p', function () {
         var reference = '#' + id;
         $(document).ready(function () {
             $(reference).click();
-        },'keyup');
+        });
 
         index = index + 1;
     }
@@ -58,49 +54,14 @@ Mousetrap.bind('command+p', function () {
         var reference = '#' + id;
         $(document).ready(function () {
             $(reference).click();
-        },'keyup');
+        });
 
         index = 0;
     }
 
-    switchFlow();
 
 });
 
-function getClassNames(){
-
-    var classnames;
-    
-    for(i = 0;i<flows.length;i++){
-
-        var name_list = flows[i].classList
-
-        for(k = 0;k<name_list.length;k++){
-            classnames = classnames + name_list[k] + " ";
-        }
-
-        classnames = classnames + "\n";
-    }
-
-    return classnames;
-}
-
-
-function switchFlow(){
-
-    for(i = 0;i<flows.length;i++){
-        if(index == i){
-            flows[i].classList.add('show');
-            flows[i].classList.add('active');
-        }
-        else{
-            flows[i].classList.remove('show');
-            flows[i].classList.remove('active');
-            
-        }
-    }
-
-}
 
 
 
