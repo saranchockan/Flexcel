6be755b9f5d71e6f4116299f4d242d95a162e817
Saran Chockan <saranjitharunesh@gmail.com>
2018-06-20 22:56:08 -0500

    disabled insertion of new columns

---
+++ js/flow-template/plan -flow template.js
@@ -0,0 +1,103 @@
+
+
+$('#1AC-Framing-tab').jexcel({
+    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+});
+
+$('#Inherency-tab').jexcel({
+    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+
+$('#Adv-1-tab').jexcel({
+    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+
+$('#Adv-2-tab').jexcel({
+    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+
+$('#Adv-3-tab').jexcel({
+    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+$('#Adv-4-tab').jexcel({
+    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+$('#Adv-5-tab').jexcel({
+    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+$('#Solvency-tab').jexcel({
+    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+$('#1NC-Framing-tab').jexcel({
+    colHeaders: ['NC', '1AR', '2NR', '2AR'],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+$('#Off-1-Tab').jexcel({
+    colHeaders: ['NC', '1AR', '2NR', '2AR'],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+
+$('#Off-2-Tab').jexcel({
+    colHeaders: ['NC', '1AR', '2NR', '2AR'],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+
+$('#Off-3-Tab').jexcel({
+    colHeaders: ['NC', '1AR', '2NR', '2AR'],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+
+$('#Off-4-Tab').jexcel({
+    colHeaders: ['NC', '1AR', '2NR', '2AR'],
+    colWidths: [ 165, 165, 165,165, 165, 165 ],
+    allowInsertColumn: false
+    
+});
+
+
+
+
+
+ 
