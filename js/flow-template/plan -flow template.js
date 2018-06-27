d65d7b91c6848227bcb59fac9d46fe14cebfd03b
Saran Chockan <saranjitharunesh@gmail.com>
2018-06-27 17:53:48 -0500

    added NC Flows (Plan-flow)

--- js/flow-template/plan -flow template.js
+++ js/flow-template/plan -flow template.js
@@ -3,8 +3,13 @@ var handstonable_flows = [];
 
 const flow_tabs = document.getElementsByClassName('tab-pane');
 var AC_tabs = document.getElementsByClassName('AC');
-var data = [
+var NC_tabs = document.getElementsByClassName('NC');
+
+
+var AC_data = [
   ['AC', '1NR', '1AR', '2NR', '2AR']
+],NC_data = [
+  ['1NC', '1AR', '2NR','2AR']
 ],
   container,
   hot;
@@ -33,14 +38,16 @@ function flowRenderer(instance, td, row, col, prop, value, cellProperties) {
   }
   
 }
-// maps function to lookup string
+
 Handsontable.renderers.registerRenderer('flowRenderer', flowRenderer);
+
+// -- Adds AC Flows
 
 for(i = 0;i<AC_tabs.length;i++){
     container = document.getElementById(AC_tabs[i].id);
 
     handstonable_flows.push(new Handsontable(container,{
-      data: data,
+      data: AC_data,
       minCols: 5,
       minRows: 20,
       colWidths: 190,
@@ -75,8 +82,48 @@ for(i = 0;i<AC_tabs.length;i++){
     }))
 }
 
+// -- Adds NC Flows
 
-// Removed All of Handsontable's licenses
+for(i = 0;i<NC_tabs.length;i++){
+  container = document.getElementById(NC_tabs[i].id);
+
+  handstonable_flows.push(new Handsontable(container,{
+    data: NC_data,
+    minCols: 4,
+    minRows: 20,
+    colWidths: 190,
+    afterSelection: function (row, col, row2, col2) {
+      var meta = this.getCellMeta(row2, col2);
+  
+      if (meta.readOnly) {
+        this.updateSettings({ fillHandle: false });
+      }
+      else {
+        this.updateSettings({ fillHandle: true });
+      }
+    },
+    cells: function (row, col) {
+      var cellProperties = {};
+      var data = this.instance.getData();
+  
+      if (row === 0) {
+        cellProperties.readOnly = true;
+      }
+      if (row === 0) {
+        cellProperties.renderer = flowLabels; // uses function directly
+      }
+  
+      else{
+        cellProperties.renderer = 'flowRenderer'; // uses lookup map
+  
+      }
+  
+      return cellProperties;
+    }
+  }))
+}
+
+// Removes All of Handsontable's licenses
 
 var allLiceneses = document.querySelectorAll("#hot-display-license-info");
 $(allLiceneses).remove();
