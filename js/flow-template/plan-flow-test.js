2c5dc81c6b83b5cb1f89490d12d1b62d270bce66
Saran Chockan <saranjitharunesh@gmail.com>
2018-06-27 01:25:36 -0500

    added diff colors for AC and NC

--- js/flow-template/plan-flow-test.js
+++ js/flow-template/plan-flow-test.js
@@ -3,7 +3,7 @@ var data = [
   ['AC', '1NR', '1AR', '2NR', '2AR']
 ],
   container,
-  hot1;
+  hot;
 
 function firstRowRenderer(instance, td, row, col, prop, value, cellProperties) {
   Handsontable.renderers.TextRenderer.apply(this, arguments);
@@ -16,10 +16,24 @@ function firstRowRenderer(instance, td, row, col, prop, value, cellProperties) {
   }
 }
 
+function flowRenderer(instance, td, row, col, prop, value, cellProperties) {
+  Handsontable.renderers.TextRenderer.apply(this, arguments);
+
+  if (col % 2 == 1) { 
+    td.style.fontWeight = 'bold';
+    td.style.color = '#076BFF'; 
+  }
+  else { 
+    td.style.fontWeight = 'bold';
+    td.style.color = 'red'; 
+  }
+  
+}
+// maps function to lookup string
+Handsontable.renderers.registerRenderer('flowRenderer', flowRenderer);
 
 container = document.getElementById('1AC-Framing-tab');
-
-hot1 = new Handsontable(container, {
+hot = new Handsontable(container, {
   data: data,
   minCols: 5,
   minRows: 20,
@@ -43,6 +57,11 @@ hot1 = new Handsontable(container, {
     }
     if (row === 0) {
       cellProperties.renderer = firstRowRenderer; // uses function directly
+    }
+
+    else{
+      cellProperties.renderer = 'flowRenderer'; // uses lookup map
+
     }
 
     return cellProperties;
