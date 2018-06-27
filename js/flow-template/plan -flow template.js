071e8e0e52f550d15505cb1c5d489e26597df8b6
Saran Chockan <saranjitharunesh@gmail.com>
2018-06-27 13:12:51 -0500

    removed jExcel

--- js/flow-template/plan -flow template.js
+++ js/flow-template/plan -flow template.js
@@ -1,122 +1,74 @@
 
-var AC_data = $.fn.jexcel('helper', { action:'createEmptyData', cols:5, rows:150 });
-var NC_data = $.fn.jexcel('helper', { action:'createEmptyData', cols:4, rows:150 });
+var data = [
+  ['AC', '1NR', '1AR', '2NR', '2AR']
+],
+  container,
+  hot;
 
+function flowLabels(instance, td, row, col, prop, value, cellProperties) {
+  Handsontable.renderers.TextRenderer.apply(this, arguments);
+  td.style.fontWeight = 'bold';
+  if (col % 2 == 1) { 
+    td.style.color = '#076BFF'; 
+  }
+  else { 
+    td.style.color = 'red'; 
+  }
+}
 
+function flowRenderer(instance, td, row, col, prop, value, cellProperties) {
+  Handsontable.renderers.TextRenderer.apply(this, arguments);
 
+  if (col % 2 == 1) { 
+    td.style.fontWeight = 'bold';
+    td.style.color = '#076BFF'; 
+  }
+  else { 
+    td.style.fontWeight = 'bold';
+    td.style.color = 'red'; 
+  }
+  
+}
+// maps function to lookup string
+Handsontable.renderers.registerRenderer('flowRenderer', flowRenderer);
 
-$('#1AC-Framing-tab').jexcel({
-    data: AC_data,
-    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
+container = document.getElementById('1AC-Framing-tab');
+hot = new Handsontable(container, {
+  data: data,
+  minCols: 5,
+  minRows: 20,
+  colWidths: 190,
+  afterSelection: function (row, col, row2, col2) {
+    var meta = this.getCellMeta(row2, col2);
+
+    if (meta.readOnly) {
+      this.updateSettings({ fillHandle: false });
+    }
+    else {
+      this.updateSettings({ fillHandle: true });
+    }
+  },
+  cells: function (row, col) {
+    var cellProperties = {};
+    var data = this.instance.getData();
+
+    if (row === 0) {
+      cellProperties.readOnly = true;
+    }
+    if (row === 0) {
+      cellProperties.renderer = flowLabels; // uses function directly
+    }
+
+    else{
+      cellProperties.renderer = 'flowRenderer'; // uses lookup map
+
+    }
+
+    return cellProperties;
+  }
 });
 
-
-$('#Inherency-tab').jexcel({
-    data: AC_data,
-    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
-
-
-$('#Adv-1-tab').jexcel({
-    data: AC_data,
-    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
-
-
-$('#Adv-2-tab').jexcel({
-    data: AC_data,
-    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
-
-$('#Adv-3-tab').jexcel({
-    data: AC_data,
-    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
-
-$('#Adv-4-tab').jexcel({
-    data: AC_data,
-    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
-
-$('#Adv-5-tab').jexcel({
-    data: AC_data,
-    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
-
-$('#Solvency-tab').jexcel({
-    data: AC_data,
-    colHeaders: ['AC', '1NR', '1AR', '2NR', '2AR' ],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
-
-
-$('#1NC-Framing-tab').jexcel({
-    data: NC_data,
-    colHeaders: ['NC', '1AR', '2NR', '2AR'],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
-
-$('#Off-1-Tab').jexcel({
-    data: NC_data,
-    colHeaders: ['NC', '1AR', '2NR', '2AR'],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
-
-
-$('#Off-2-Tab').jexcel({
-    data: NC_data,
-    colHeaders: ['NC', '1AR', '2NR', '2AR'],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
-
-
-$('#Off-3-Tab').jexcel({
-    data: NC_data,
-    colHeaders: ['NC', '1AR', '2NR', '2AR'],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
-
-
-$('#Off-4-Tab').jexcel({
-    data: NC_data,
-    colHeaders: ['NC', '1AR', '2NR', '2AR'],
-    colWidths: [ 165, 165, 165,165, 165, 165 ],
-    allowInsertColumn: false
-    
-});
+document.getElementById('hot-display-license-info').style.visibility = "hidden";
 
 
 
-
-
- 
