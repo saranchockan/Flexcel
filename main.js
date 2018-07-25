7da10310275e764f4aaa8e6caa4ab4247c2d5ee1
Saran Chockan <saranjitharunesh@gmail.com>
2018-07-25 18:07:02 -0500

    fixed quit bugs

--- main.js
+++ main.js
@@ -43,6 +43,8 @@ function createWindow() {
       });
     if (choice == 1) {
       e.preventDefault();
+      mainWindow.setResizable(false);
+      mainWindow.setMaximizable(false);
     }
   });
 
